bb93d0078b7d956938e6834a2b5c5cbc18c6bd7e
Tria <Tria@TRIA-PC>
2019-12-22 20:42:50 +0100

    Added basic Attributes.

---
+++ PreferenceInvestigator.Model/TypeAttributes/UnsignedIntegerAttribute.cs
@@ -0,0 +1,13 @@
+﻿using PreferenceInvestigator.Model.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreferenceInvestigator.Model.TypeAttributes
+{
+    public class UnsignedIntegerAttribute : PreferenceAttribute
+    {
+    }
+}
