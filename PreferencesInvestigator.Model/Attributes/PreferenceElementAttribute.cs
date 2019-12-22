ad456e848c794c08a1a26260d7e6074dd64ebcec
Tria <Tria@TRIA-PC>
2019-12-22 20:18:23 +0100

    Initial Commit.

---
+++ PreferencesInvestigator.Model/Attributes/PreferenceElementAttribute.cs
@@ -0,0 +1,57 @@
+﻿using PreferenceInvestigator.Model.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+
+namespace PreferenceInvestigator.Model.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class PreferenceElementAttribute : Attribute, IPreferenceElement
+    {
+        private string _title;
+        private int _displayIndex;
+        private bool _userCanChange;
+        private string _category;
+        private string _description;
+
+        public List<string> CategoryTree => _category?.Split(Path.DirectorySeparatorChar).ToList();
+
+        public PreferenceElementAttribute(string title)
+        {
+            DisplayName = title;
+            DisplayIndex = 0;
+            UserCanChange = false;
+            Category = String.Empty;
+            Description = String.Empty;
+        }
+
+        public string DisplayName
+        {
+            get { return _title; }
+            private set { _title = value; }
+        }
+        public int DisplayIndex
+        {
+            get { return _displayIndex; }
+            set { _displayIndex = value; }
+        }
+        public bool UserCanChange
+        {
+            get { return _userCanChange; }
+            set { _userCanChange = value; }
+        }
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }        
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+    }
+}
--- PreferencesInvestigator.Model/Interfaces/IPreferenceAttribut.cs
+++ PreferencesInvestigator.Model/Interfaces/IPreferenceAttribut.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PreferencesInvestigator.Model
+namespace PreferenceInvestigator.Model.Interfaces
 {
-    public class Class1
+    public interface IPreferenceAttribut
     {
     }
 }
